9d3abdd26577e95a0599f2cce9361dfc06cc17d6
Sergey Zhdanov <sergeyk929@mail.ru>
2021-02-02 08:57:41 +0700

    start main components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<div className='header'>
+  		<img src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png" width='50' alt="" />
+  	</div>
+	)
+}
+
+export default Header
